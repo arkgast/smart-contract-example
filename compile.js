0eb7f9ea63d2548be3dbb9e13b2d1f1e6b1c5c65
Arnold Gandarillas Castillo <arkgast@gmail.com>
2019-02-03 10:56:08 -0500

    build: add tools

--- compile.js
+++ compile.js
@@ -6,23 +6,23 @@ const contractFile = path.resolve(__dirname, 'contracts', 'inbox.sol')
 const source = fs.readFileSync(contractFile, 'utf8')
 
 const input = {
-	language: 'Solidity',
-	sources: {
-		'inbox.sol': {
+  language: 'Solidity',
+  sources: {
+    'inbox.sol': {
       content: source
     }
-	},
-	settings: {
+  },
+  settings: {
     optimizer: {
       enabled: true,
       runs: 100
     },
-		outputSelection: {
-			'inbox.sol': {
-				'Inbox': [ 'abi' , 'evm.bytecode']
-			}
-		}
-	}
+    outputSelection: {
+      'inbox.sol': {
+        'Inbox': [ 'abi', 'evm.bytecode' ]
+      }
+    }
+  }
 }
 
 const compiledContract = JSON.parse(solc.compile(JSON.stringify(input)))
