a65b9172dada0ac4df88ad7d465f2bcaf88794f3
Arnold Gandarillas Castillo <arkgast@gmail.com>
2019-02-08 09:22:51 -0500

    feat: implement lottery smart contract

--- compile.js
+++ compile.js
@@ -2,13 +2,13 @@ const path = require('path')
 const fs = require('fs')
 const solc = require('solc')
 
-const contractFile = path.resolve(__dirname, 'contracts', 'inbox.sol')
+const contractFile = path.resolve(__dirname, 'contracts', 'lottery.sol')
 const source = fs.readFileSync(contractFile, 'utf8')
 
 const input = {
   language: 'Solidity',
   sources: {
-    'inbox.sol': {
+    'lottery.sol': {
       content: source
     }
   },
@@ -18,15 +18,15 @@ const input = {
       runs: 100
     },
     outputSelection: {
-      'inbox.sol': {
-        'Inbox': [ 'abi', 'evm.bytecode' ]
+      'lottery.sol': {
+        'Lottery': [ 'abi', 'evm.bytecode' ]
       }
     }
   }
 }
 
 const compiledContract = JSON.parse(solc.compile(JSON.stringify(input)))
-const { abi, evm } = compiledContract.contracts['inbox.sol'].Inbox
+const { abi, evm } = compiledContract.contracts['lottery.sol'].Lottery
 
 module.exports = {
   abi,
